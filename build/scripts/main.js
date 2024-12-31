bb8c1b4f8e5cc4d5e4a22af921b35019504116a4
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-12-31 17:10:53 -0300

    fix: Gulp version to imagemin

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,1 +1,1 @@
-function ಠ_ಠ920(){console.log("Olá Mundo!")}function ಠ_ಠ921(o=6){console.log(Math.floor(Math.random()*o)+1)}ಠ_ಠ920(),ಠ_ಠ921(),module.ಠ_ಠ922={ಠ_ಠ920:ಠ_ಠ920,ಠ_ಠ921:ಠ_ಠ921};
+function ಠ_ಠ265(){console.log("Olá Mundo!")}function ಠ_ಠ266(o=6){console.log(Math.floor(Math.random()*o)+1)}ಠ_ಠ265(),ಠ_ಠ266(),module.ಠ_ಠ267={ಠ_ಠ265:ಠ_ಠ265,ಠ_ಠ266:ಠ_ಠ266};
