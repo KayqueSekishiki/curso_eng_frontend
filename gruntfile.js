efdf9639fb3d9a8b5c134708591231005b2150f8
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-01-03 20:34:30 -0300

    create task default

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,18 @@
+const { log } = require("grunt");
+
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
   });
+
+  grunt.registerTask("olaGrunt", function () {
+    const done = this.async();
+
+    setTimeout(function () {
+      console.log("ola grunt");
+      done();
+    }, 4000);
+  });
+
+  grunt.registerTask("default", ["olaGrunt"]);
 };
