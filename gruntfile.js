25fef6c5228771cde9b0e7de2953fa2375db39dc
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-01-03 20:50:35 -0300

    remove task

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,24 @@
-const { log } = require("grunt");
-
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    less: {
+      development: {
+        files: {
+          "./build/styles/main.css": "./src/styles/main.less",
+        },
+        production: {
+          options: {
+            compress: true,
+          },
+          files: {
+            "./build/styles/main.min.css": "./src/styles/main.less",
+          },
+        },
+      },
+    },
   });
 
-  grunt.registerTask("olaGrunt", function () {
-    const done = this.async();
+  grunt.loadNpmTasks("grunt-contrib-less");
 
-    setTimeout(function () {
-      console.log("ola grunt");
-      done();
-    }, 4000);
-  });
-
-  grunt.registerTask("default", ["olaGrunt"]);
+  grunt.registerTask("default", ["less"]);
 };
