d1a6a0000aae66738a6920f02a57427820ae9d10
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-01-14 14:30:08 -0300

    Handling exceptions

--- main.js
+++ main.js
@@ -9,19 +9,47 @@ $(document).ready(function () {
     $(this).find("i").addClass("d-none");
     $(this).find("span").removeClass("d-none");
 
-    $.ajax(endpoint).done((res) => {
-      console.log(res);
-      const publicPlace = res.logradouro;
-      const neighborhood = res.bairro;
-      const city = res.localidade;
-      const state = res.estado;
-      const address = `${publicPlace}, ${neighborhood} - ${city} - ${state}`;
-      $("#address").val(address);
+    // $.ajax(endpoint).done((res) => {
+    //   console.log(res);
+    //   const publicPlace = res.logradouro;
+    //   const neighborhood = res.bairro;
+    //   const city = res.localidade;
+    //   const state = res.estado;
+    //   const address = `${publicPlace}, ${neighborhood} - ${city} - ${state}`;
+    //   $("#address").val(address);
+    // });
 
-      setTimeout(() => {
-        $(button).find("i").removeClass("d-none");
-        $(button).find("span").addClass("d-none");
-      }, 2000);
-    });
+    // setTimeout(() => {
+    //   $(button).find("i").removeClass("d-none");
+    //   $(button).find("span").addClass("d-none");
+    // }, 2000);
+
+    fetch(endpoint)
+      .then((res) => {
+        console.log(res);
+
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+
+        const publicPlace = json.logradouro;
+        const neighborhood = json.bairro;
+        const city = json.localidade;
+        const state = json.estado;
+        const address = `${publicPlace}, ${neighborhood} - ${city} - ${state}`;
+        $("#address").val(address);
+      })
+      .catch((error) => {
+        alert(
+          "Ocorreu um erro ao buscar o endereço, tente novamente mais tarde."
+        );
+      })
+      .finally(() => {
+        setTimeout(() => {
+          $(button).find("i").removeClass("d-none");
+          $(button).find("span").addClass("d-none");
+        }, 2000);
+      });
   });
 });
