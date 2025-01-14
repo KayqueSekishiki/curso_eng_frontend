589b299b530038f560ffc9227f17ed171c7d54f5
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-01-14 17:32:15 -0300

    Monster

--- main.js
+++ main.js
@@ -1,30 +1,57 @@
-$(document).ready(function () {
-  const name = document.querySelector(".profile-name");
-  const username = document.querySelector(".profile-username");
-  const avatar = document.querySelector(".profile-avatar");
-  const repos = document.querySelector("#repos");
-  const followers = document.querySelector("#followers");
-  const following = document.querySelector("#following");
-  const link = document.querySelector(".profile-link");
+// Função construtora base (abstração)
+function Monster(name, type, hitPoints) {
+  this.name = name;
+  this.type = type;
+  this.hitPoints = hitPoints;
+}
 
-  const endpoint = "https://api.github.com/users/kayquesekishiki";
+Monster.prototype.attack = function () {
+  console.log(`${this.name} ataca com fúria!`);
+};
 
-  fetch(endpoint)
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      name.innerText = json.name;
-      username.innerText = "@" + json.login;
-      avatar.src = json.avatar_url;
-      repos.innerText = json.public_repos;
-      followers.innerText = json.followers;
-      following.innerText = json.following;
-      link.href = json.html_url;
-    })
-    .catch((error) => {
-      alert(
-        "Ocorreu um erro ao buscar este perfil no github, por favor, tente novamente mais tarde."
-      );
-    });
-});
+Monster.prototype.takeDamage = function (amount) {
+  this.hitPoints -= amount;
+  console.log(
+    `${this.name} sofreu ${amount} de dano e agora tem ${this.hitPoints} pontos de vida.`
+  );
+  if (this.hitPoints <= 0) {
+    console.log(`${this.name} foi derrotado!`);
+  }
+};
+
+function Dragon(name, hitPoints, element) {
+  Monster.call(this, name, "Dragão", hitPoints);
+  this.element = element;
+}
+
+Dragon.prototype = Object.create(Monster.prototype);
+Dragon.prototype.constructor = Dragon;
+
+Dragon.prototype.attack = function () {
+  console.log(`${this.name}, o dragão de ${this.element}, cospe fogo!`);
+};
+
+function Goblin(name, hitPoints, weapon) {
+  Monster.call(this, name, "Goblin", hitPoints);
+  this.weapon = weapon;
+}
+
+Goblin.prototype = Object.create(Monster.prototype);
+Goblin.prototype.constructor = Goblin;
+
+Goblin.prototype.attack = function () {
+  console.log(
+    `${this.name}, o goblin armado com um(a) ${this.weapon}, golpeia com rapidez!`
+  );
+};
+
+const smaug = new Dragon("Smaug", 300, "fogo");
+const grob = new Goblin("Grob", 50, "adaga enferrujada");
+const minion = new Monster("Lacaio", "Desconhecido", 100);
+
+smaug.attack();
+grob.attack();
+minion.attack();
+
+grob.takeDamage(30);
+smaug.takeDamage(310);
