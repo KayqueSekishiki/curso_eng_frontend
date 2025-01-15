8a37f809f5f615c5abc6a11286ecaf8c981653b8
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-01-15 03:53:43 -0300

    typescript

---
+++ main.js
@@ -0,0 +1,4 @@
+var multiply = function (a, b) { return a * b; };
+var greetin = function (name) { return "Ol\u00E1 ".concat(name); };
+console.log(multiply(2, 3));
+console.log(greetin("Kayque"));
---
+++ main.ts
@@ -0,0 +1,9 @@
+const multiply = (a: number, b: number): number => a * b;
+
+const greetin = (name: string): string => `Olá ${name}`;
+
+console.log(multiply(2, 3));
+console.log(greetin("Kayque"));
+
+//use o comando npm run dev para gerar o arquivo main.js
+//use o comando node main.js para executar o arquivo main.js
